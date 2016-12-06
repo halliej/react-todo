45e02d694877afdb1792aa980f29ea725d3880d1
Hal DeLage <hal.delage@teamaol.com>
2016-12-06 16:33:09 -0500

    Move firebase creds to env

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,14 +3,14 @@ import firebase from 'firebase';
 // Initialize Firebase
 try {
   var config = {
-    apiKey: "AIzaSyBFvsmx-6h-7M2kkN90qrCy_WUWWQaKNRg",
-    authDomain: "hal-todo-app.firebaseapp.com",
-    databaseURL: "https://hal-todo-app.firebaseio.com",
-    storageBucket: "hal-todo-app.appspot.com",
-    messagingSenderId: "208651802092"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
 } catch (e) {
-
+  console.log(e);
 }
 
 firebase.initializeApp(config);
